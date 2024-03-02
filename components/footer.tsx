7261e98a39d7058788f94a8980f419db9a71f908
XxJaviMadxX <javierortizmi@gmail.com>
2024-03-02 17:25:58 -0500

    Update Copy

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,8 +9,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; {new Date().getFullYear()} Javier Ortiz Millan.{" "}
-        {dictionary.footerData.rights}
+        Javier Ortiz Millan.{" "}
       </small>
       <p className="text-xs mb-2">
         <span className="font-semibold">
